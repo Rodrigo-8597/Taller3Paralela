946ac41a095e9e06b7d5b187819af3486fe7a199
Rodrigo Carmona Ramírez <63922169+Rodrigo-8597@users.noreply.github.com>
2020-05-30 16:14:45 -0400

    Update puntajes.dart

--- puntajes.dart
+++ puntajes.dart
@@ -4,50 +4,42 @@ import 'dart:convert';
 import 'dart:math';
 
 void main(){
-  new File('puntajes.csv').readAsString().then((String contents) {
+  new File('file2.csv').readAsString().then((String contents) {
     var separar = contents.split(";");
     int tt = separar.length;
     for(int bb = 1; bb < 7; bb++){
       double promedio = calcular_promedio(separar,tt,bb); //Variable que guardara el promedio de los nems hasta el promedio de los resultados en historia
       double desviacion_est = calcular_desv_est(separar,tt,bb,promedio); //Variable que guardara la desviacion estandar de los nems hasta el de los resultados en historia
       double mediana = calcular_mediana(separar,tt,bb); //Variable que guardara la mediana de los nems hasta la mediana de los resultados en historia
-      int moda = calcular_moda(separar,tt,bb); //Variable que guardara la moda de los nems hasta la de los resultados en historia
-      mostrar(bb,promedio,desviacion_est,mediana,moda); //Funcion que mostrara en pantalla los resultados anteriores
+      mostrar(bb,promedio,desviacion_est,mediana); //Funcion que mostrara en pantalla los resultados anteriores
+      calcular_moda(separar,tt,bb);
     }
     integrantes();
   }); 
 }
 
 double calcular_promedio(var hh, int numero, int ind){
-  var dato = [1]; //Arreglo donde se guardaran todos los resultados de cada nem
-  while(ind < numero){
-    var ent = int.parse(hh[ind]); //Transforma el dato a un valor int 
-    dato.add(ent); //Se almacena dentro del arreglo
-    ind = ind + 7; //Se aumenta en 7 unidades para ir iterando por cada nem
-  }
   int suma = 0; //Suma de todos los nems registrados, inicializada en 0
   int n = 0; //Numero de valores (Xi), inicializado en 0
-  for(int f = 1; f < dato.length; f++){
-    suma += dato[f];
+  while(ind < numero){
+    var ent = int.parse(hh[ind]); //Transforma el dato a un valor int 
+    suma += ent;
     n++;
+    ind = ind + 7; //Se aumenta en 7 unidades para ir iterando por cada nem
   }
   double promedio = suma/n;
   return promedio;
 }
 
 double calcular_desv_est(var hh, int nn, int indice, double prom){
-  var dato = [1]; //Se repite el proceso de obtencion de datos de la funcion calcular_promedio (lineas 21-26)
-  while(indice < nn){
-    var ent = int.parse(hh[indice]);
-    dato.add(ent);
-    indice = indice + 7;
-  }
   double suma = 0; //Variable que almacenara el resultado de la sumatoria, inicializada en 0
   double n = 0; //Numero de valores (Xi), inicializado en 0
-  for(int e = 1; e < dato.length; e++){ //En el for se realiza el proceso que calcula la sumatoria 
-    double gg = dato[e] - prom; //todos los valores (Xi) se restan con el promedio
+  while(indice < nn){
+    var entero = int.parse(hh[indice]);
+    double gg = entero - prom; //todos los valores (Xi) se restan con el promedio
     suma += pow(gg,2); //el resultado de dicha operacion se eleva al cuadrado, operacion acumulativa
     n++;
+    indice = indice + 7;
   }
   double k = suma/(n-1); //Hacemos la division (aplicando la formula que se encuentra dentro de la raiz)
   double des_est = pow(k,0.5); //Aplicamos raiz cuadrada del resultado anterior, aqui se obtiene la desviacion estandar
@@ -55,106 +47,102 @@ double calcular_desv_est(var hh, int nn, int indice, double prom){
 }
 
 double calcular_mediana(var ff, int numero, int ind){
-  var dato = [1]; //Se repite el proceso de obtencion de datos de la funcion calcular_promedio (lineas 21-26)
-  int contador = 0;
-  //var mediana;
-  while(ind < numero){
-    var ent = int.parse(ff[ind]);
-    dato.add(ent);
-    ind = ind + 7;
-    contador++;
-  }
+  int contador = 0; //Contador hara la funcion de contar la cantidad de columnas del archivo csv
   var aux;
-  for(int d = 1; d < (dato.length-1); d++){ //Se aplica el algoritmo de ordenamiento burbuja, para que ordene los datos (Xi) de menor a mayor
-    for(int r = 1; r < (dato.length-1); r++){
-      if(dato[r] > dato[r+1]){
-        aux = dato[r];
-        dato[r] = dato[r+1];
-        dato[r+1] = aux;
+  for(int d = ind; d < numero; d = d + 7){ //Se aplica el algoritmo de ordenamiento burbuja, para que ordene los datos (Xi) de menor a mayor
+    for(int r = ind; r < numero; r = r + 7){
+      int n1 = int.parse(ff[d]);
+      int n2 = int.parse(ff[r]);
+      if(n1 > n2){
+        aux = ff[d];
+        ff[d] = ff[r];
+        ff[r] = aux;
       }
     }
+    contador++;
   }
   if(contador%2 != 0){ //Si el numero de datos es impar, entonces buscamos el valor que se encuentra justo en el medio
-    var largo = dato.length; //En realidad, el largo del array dato es par (dado que siempre al inicio se agrega un valor inicial (1) )
-    var pos = (largo/2); //Dividimos el numero total de datos registrados a la mitad
-    var pos_int = pos.toInt(); //Por si acaso, transformamos el numero a un int
-    var mediana_impar = dato[pos_int].toDouble(); //Transformamos el valor escogido a un double (dado que la funcion tiene que retornar un valor double)
+    var largo = contador-1; //Para que el resultado de la variable de la siguiente linea, le quitamos 1 unidad al contador 
+    var pos = (largo/2).toInt(); //Dividimos el numero total de datos registrados a la mitad
+    int indice = (pos * 7) + ind;
+    var numeros = int.parse(ff[indice]);
+    var mediana_impar = numeros.toDouble(); //Transformamos el valor escogido a un double (dado que la funcion tiene que retornar un valor double)
     return mediana_impar;
   }
 
-  if(contador%2 == 0){ //Si es par, entonces hay dos valores en el medio (En realidad, el largo del array es impar, por la misma razon que la linea 77)
-    var numm = (contador/2); //Obtenemos uno de los valores (se calcula dividiendo por la posicion del ultimo dato del array dato, representado en la variable contador)
-    var num1 = numm.toInt();
-    var num2 = num1 + 1; //El otro valor intermedio se obtiene aumentando el resultado de la variable en una unidad
-    var nn = dato[num1].toDouble(); //Ambos valores se transforman a numeros de tipo double, para que el resultado de la division no salga un error
-    var nv = dato[num2].toDouble();
+  if(contador%2 == 0){ //Si es par, entonces hay dos valores en el medio 
+    var nu1 = (contador/2).toInt(); //Obtenemos uno de los valores (se calcula dividiendo la cantidad de columnas a la mitad, representado en la variable contador)
+    var nu2 = nu1 + 1; //El otro valor intermedio se obtiene aumentando el resultado de la variable en una unidad
+    int indice1 = ((nu1 - 1) * 7) + ind;
+    int indice2 = ((nu2 - 1) * 7) + ind;
+    var num1 = int.parse(ff[indice1]);
+    var num2 = int.parse(ff[indice2]);
+    var nn = num1.toDouble(); //Ambos valores se transforman a numeros de tipo double, para que el resultado de la division no salga un error
+    var nv = num2.toDouble();
     var mediana_par = ((nn+nv)/2).toDouble(); //Los dos valores que se encuentran en el medio se suman, posteriormente el resultado del dividendo se divide en dos
     return mediana_par;
   }
 }
 
-int calcular_moda(var datos, int numer, int ind){
-  var dato = [1]; //Se repite el proceso de obtencion de datos de la funcion calcular_promedio (lineas 21-26)
-  int contador_p = 0; //Variable que guardara el total de numeros unicos (no se cuentan los repetidos)
+void calcular_moda(var datos, int numer, int ind){
   var moda = ""; //Variable que guardara la(s) moda(s), puede haber mas de una
-  while(ind < numer){
-    var ent = int.parse(datos[ind]);
-    dato.add(ent);
-    ind = ind + 7;
-  }
-  var numeros = [1]; //Array que guardara los numeros unicos encontrados durante la recoleccion de datos
-  var indices = [1]; //Array que guardara las posiciones donde fueron encontrados los numeros unicos
   var repeticion = [0]; //Array que guardara el numero de veces que se repite cada dato encontrado
   int mas_alto = 1; //Variable que almacenara la cantidad maxima de oportunidades que se repite un numero
-  for(int i = 1; i < dato.length; i++){ 
-		if(i==1){ //El primer dato se guarda automaticamente en el array numeros
-			int numerito = dato[i].toInt();
-      numeros.add(numerito);
-      indices.add(i.toInt());
-			contador_p += 1;
-    }
-    if(i > 1){
-      int var_random = 0;
-      for(int u = 1; u < i; u++){
-        if(dato[u] == dato[i]){ //El for y posterior if comprueba si el numero de la iteracion se encuentra repetido en el array numeros
-          var_random = 1; //Si var_random = 1, quiere decir que efectivamente el numero de la iteracion es un dato repetido
-        }
-        if(var_random == 0){ //Si el valor de var_random no cambio a 1, entonces se ha encontrado un dato diferente (no repetido)
-          int numerito = dato[i].toInt();
-          numeros.add(numerito); //Entonces, el numero valor unico encontrado se almacena en su array dedicado
-          indices.add(i.toInt()); //Tambien se almacena la posicion donde fue encontrado
-          contador_p += 1;
-          break;
-        }
-      }
-		}
-	}
-  for(int e = 1; e < indices.length; e++){
+  //var modas = [1];
+  for(int e = ind; e < numer; e = e + 7){
     int contador_letras = 0; //Esta variable permite calcular el numero de veces que se repite cada dato unico (Xi)
-    for(int w = 1; w < numeros.length; w++){
-      if(numeros[w] == numeros[e]){ //Los for permiten realizar la comparacion de cada valor unico y comprobar la cantidad de veces que se repite
+    for(int w = ind; w < numer; w = w + 7){
+      int n1 = int.parse(datos[e]);
+      int n2 = int.parse(datos[w]);
+      if(n1 == n2){ //Los for permiten realizar la comparacion de cada valor unico y comprobar la cantidad de veces que se repite
         contador_letras++;
       }
     }
     repeticion.add(contador_letras.toInt());
   }
   for(int rr = 1; rr < repeticion.length; rr++){
-    for(int ww = 1; ww < repeticion.length; ww++){
-      if((repeticion[rr] > repeticion[ww]) & (repeticion[rr] > mas_alto)){ //Este algoritmo permite comparar el numero de repeticiones de cada dato para saber cual es el mas alto (cantidad maxima de repeticiones de un valor(es))
-        mas_alto = repeticion[rr].toInt(); //Si los condicionantes del if son verdaderos, entonces se ha encontrado un valor de repeticiones mas alto, dicho valor se almacena en la variable mas_alto
-      }
+    if(repeticion[rr] > mas_alto){ //Este algoritmo permite comparar el numero de repeticiones de cada dato para saber cual es el mas alto (cantidad maxima de repeticiones de un valor(es))
+      mas_alto = repeticion[rr].toInt(); //Si los condicionantes del if son verdaderos, entonces se ha encontrado un valor de repeticiones mas alto, dicho valor se almacena en la variable mas_alto
     }
   }
+  //int nun = 1;
   for(int mm = 1; mm < repeticion.length; mm++){ //Si hay varios valores (mas de uno) que se repiten la misma cantidad de ocasiones, entonces se guardaran en una sola variable
-    var hg = numeros[mm].toString();
-    moda += hg;
-    break; //El break lo ejecutamos en caso de que solo querramos un valor, que representara la moda obtenida
+    if(repeticion[mm] == mas_alto){
+      int indice = ((mm-1)*7) + ind; 
+      var hg = datos[indice].toString();
+      moda += hg;
+      break;
+      //var hg = int.parse(datos[indice]);
+      /*
+      if(nun == 1){
+        modas.add(hg);
+        nun++;
+      }
+      if(nun > 1){
+        int var_random = 0;
+        for(int jj = 1; jj < modas.length; jj++){
+          if(modas[jj] == hg){
+            var_random = 1;
+          }
+        }
+        if(var_random == 0){
+          modas.add(hg);
+          nun++;
+        }
+      }
+      */
+    }
   }
-  var modas = int.parse(moda);
-  return modas;
+  /*
+  for(int rt = 1; rt < modas.length; rt++){
+    moda += modas[rt].toString();
+    moda += " ";
+  }
+  */
+  print('Moda: $moda');
 }
 
-void mostrar(int posicion, double prom, double des, double med, int mod){ //Funcion void que mostrara los resultados obtenidos solicitados
+void mostrar(int posicion, double prom, double des, double med){ //Funcion void que mostrara los resultados obtenidos solicitados
   if(posicion == 1){
     print('=== Nem ===');
   }
@@ -176,7 +164,6 @@ void mostrar(int posicion, double prom, double des, double med, int mod){ //Func
   print('Promedio: $prom');
   print('Desviacion Estandar: $des');
   print('Mediana: $med');
-  print('Moda: $mod');
 }
 
 void integrantes(){
